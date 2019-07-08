d9ba0c6a1ab9d3492970d02473e073d144385c89
Scott Halgrim <shalgrim@gmail.com>
2019-07-07 21:35:35 -0700

    fourth test passes now

--- my_first_async_io.js
+++ my_first_async_io.js
@@ -1,10 +1,16 @@
 var fs = require('fs')
+var num_newlines = undefined
 
-var txt = fs.readFile(process.argv[2], function(err, data) {
-    return data.toString()
-})
+function print_newlines() {
+    console.log(num_newlines)
+}
 
-var num_lines = txt.split('\n')
-var num_newlines = num_lines.length - 1
+function count_newlines(filename, printer) {
+    fs.readFile(filename, function named_function(err, data) {
+        var num_lines = data.toString().split('\n').length
+        num_newlines = num_lines - 1
+        printer()
+    })
+}
 
-console.log(num_newlines)
+count_newlines(process.argv[2], print_newlines)
